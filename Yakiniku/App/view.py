983b5d823cc1f5542257d8d7daf6d54c1f1cd64e
李玮祺 <2016011291@secoder.net>
2019-06-10 21:58:32 +0800

    3

--- Yakiniku/App/view.py
+++ Yakiniku/App/view.py
@@ -58,10 +58,13 @@ def show(request):
             key = "trans" + str(info.id)
             info.trans = concat[key]
             key = "bold" + str(info.id)
+            print(concat.getlist(key))
             if (concat.getlist(key)):
                 info.bold = False
+                print("f")
             else:
                 info.bold = True
+                print("t")
         request.session['infos'] = infos
         yaki.renew(infos, settings.UPLOAD_ROOT + '/' + picname, settings.RESULT_ROOT + '/s_' + picname, settings.RESULT_ROOT + '/' + picname)
 
